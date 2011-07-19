87c4a7cd4123031c0ec0f957133224c82d2eb8bd
Michal Seta <mis@artengine.ca>
2011-07-19 18:27:09 -0400

    log panel class derived from wxTextCtrl

---
+++ src/prototypes/wxgui/logpanel.h
@@ -0,0 +1,14 @@
+#include <wx/wx.h>
+#include <string>
+
+class LogPanel : public wxTextCtrl
+{
+public:
+	//LogPanel();
+	LogPanel (wxWindow *parent,wxWindowID id, wxString string="", const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxTE_MULTILINE | wxTE_READONLY | wxHSCROLL)
+		: wxTextCtrl(parent, id, string, pos, size, style ) {}
+	//wxPanel *m_parent;
+	void writeLine(std::string line);
+};
+
+
